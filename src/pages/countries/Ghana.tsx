027c787cec8975c13d4888a49175986b387cdc5c
John-Dag Addy <jaedagy@gmail.com>
2022-06-23 18:41:51 +0000

    fix: changed arrow function

--- src/pages/countries/Ghana.tsx
+++ src/pages/countries/Ghana.tsx
@@ -3,7 +3,7 @@ import { Box, Grid, Text, Heading, VStack } from '@chakra-ui/react'
 import ChakraBox from 'components/ChakraBox'
 import useCustomColorMode from '../../hooks/useCustomColorMode'
 
-function Ghana() {
+const Ghana = () => {
   const { bg, brand } = useCustomColorMode()
 
   return (
