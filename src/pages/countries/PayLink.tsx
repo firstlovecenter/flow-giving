af29bed79fcf9ce86d492a2ccb8f37a54a11a6c1
John-Dag Addy <jaedagy@gmail.com>
2022-06-24 10:34:58 +0000

    feat: addedd code for redirecting to paylink

---
+++ src/pages/countries/PayLink.tsx
@@ -0,0 +1,20 @@
+import { Spinner, Text, VStack } from '@chakra-ui/react'
+import useCustomColorMode from 'hooks/useCustomColorMode'
+
+const Other = () => {
+  const { brand } = useCustomColorMode()
+  const openPayLink = () => {
+    window.location.href = 'https://paylink.today/flctownch/'
+    return null
+  }
+
+  return (
+    <VStack height="75vh" alignItems="center" justifyContent="center">
+      {openPayLink()}
+      <Spinner size="xl" color={brand} />
+      <Text>Please wait while we redirect you for payment</Text>
+    </VStack>
+  )
+}
+
+export default Other
