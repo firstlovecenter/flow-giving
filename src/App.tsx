beeae134e3f86d83f63e3f6b0bb1c30ffa870888
John-Dag Addy <jaedagy@gmail.com>
2023-03-25 21:16:57 +0000

    fix: try something to redirect users to ghana page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom'
 import theme from './utils/theme'
 import LandingPage from './pages/LandingPage'
 import Ghana from './pages/countries/Ghana'
@@ -10,21 +10,36 @@ import Navigation from './pages/Navigation'
 import Other from './pages/countries/Other'
 import BackButton from './pages/BackButton'
 
-const App = () => (
-  <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <BackButton />
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/ghana" element={<Ghana />} />
-        <Route path="/uk" element={<UK />} />
-        <Route path="/usa" element={<USA />} />
-        <Route path="/other" element={<Other />} />
-        <Route path="*" element={<Ghana />} />
-      </Routes>
-    </BrowserRouter>
-  </ChakraProvider>
-)
+const App = () => {
+  const location = useLocation()
+  if (location.pathname === '/ghana') {
+    return (
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <BackButton />
+          <Navigation />
+          <Ghana />
+        </BrowserRouter>
+      </ChakraProvider>
+    )
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <BackButton />
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/ghana" element={<Ghana />} />
+          <Route path="/uk" element={<UK />} />
+          <Route path="/usa" element={<USA />} />
+          <Route path="/other" element={<Other />} />
+          <Route path="*" element={<Ghana />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
+  )
+}
 
 export default App
